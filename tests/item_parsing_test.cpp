2fce3b1b1099e97f170af4f60bb720db982724ac
ThonkDifferent <41342923+thonkdifferent@users.noreply.github.com>
2023-11-04 16:37:53 +0200

    sync

---
+++ tests/item_parsing_test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+namespace Sugar::CLI{
+    TEST(item_parsing, accept_string_value)
+    {
+        item<std::string> test_item = item
+    }
+    TEST(item_parsing, accept_integer_value)
+    {
+
+    }
+    TEST(item_parsing, accept_floating_point_value)
+    {
+
+    }
+    TEST(item_parsing, accept_boolean_value_with_just_header)
+    {
+
+    }
+    TEST(item_failiure_callback, call_when_required_value_is_not_found)
+    {
+
+    }
+    TEST(item_failiure_callback, call_when_int_is_mallformed)
+    {
+
+    }
+    TEST(item_failiure_callback, call_when_float_is_mallformed)
+    {
+
+    }
+}
